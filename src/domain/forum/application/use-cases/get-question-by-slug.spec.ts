fb51116e93cd0a96db4b535fc4bb947188d2768a
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-12-11 22:26:08 -0300

    added faker and fixed type module

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,27 +1,30 @@
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
-import { makeQuestion } from "test/factory/make-question";
+import { makeQuestion } from "test/factories/make-question";
+import { Slug } from "../../enterprise/entities/value-objects/slug";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let getQuestionBySlugUseCase: GetQuestionBySlugUseCase;
 
 describe("Get question by slug", () => {
-  beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
-    getQuestionBySlugUseCase = new GetQuestionBySlugUseCase(
-      inMemoryQuestionsRepository
-    );
-  });
+	beforeEach(() => {
+		inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+		getQuestionBySlugUseCase = new GetQuestionBySlugUseCase(
+			inMemoryQuestionsRepository,
+		);
+	});
 
-  it("should be able to get a question by slug", async () => {
-    const newQuestion = makeQuestion()
+	it("should be able to get a question by slug", async () => {
+		const newQuestion = makeQuestion({
+			slug: Slug.create("example-question-slug"),
+		});
 
-    await inMemoryQuestionsRepository.create(newQuestion);
+		await inMemoryQuestionsRepository.create(newQuestion);
 
-    const { question } = await getQuestionBySlugUseCase.execute({
-      slug: "example-question-slug",
-    });
+		const { question } = await getQuestionBySlugUseCase.execute({
+			slug: "example-question-slug",
+		});
 
-    expect(question.slug.value).toEqual("example-question-slug");
-  });
+		expect(question.slug.value).toEqual("example-question-slug");
+	});
 });
--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,24 @@
+import { faker } from "@faker-js/faker";
+
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import {
 	Question,
 	type QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-	const question = Question.create({
-		authorId: new UniqueEntityID(),
-		title: "Example question",
-		slug: Slug.create("example-question-slug"),
-		content: "Example question content",
-		...override
-	});
+export function makeQuestion(
+	override: Partial<QuestionProps> = {},
+	id?: UniqueEntityID,
+) {
+	const question = Question.create(
+		{
+			authorId: new UniqueEntityID(),
+			title: faker.lorem.sentence(),
+			content: faker.lorem.text(),
+			...override,
+		},
+		id,
+	);
 
 	return question;
 }
